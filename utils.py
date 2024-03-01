c0ca206d3bcbfc5eccfd4d1b48d7328f6520d001
Pullela Yaswant Sai Krishna <144750215+yaswant-krishna@users.noreply.github.com>
2024-03-01 22:20:38 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -64,3 +64,13 @@ def test(model, device, test_loader, criterion):
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
      
+def visualize_loss_accuracy():
+    fig, axs = plt.subplots(2,2,figsize=(15,10))
+    axs[0, 0].plot(train_losses)
+    axs[0, 0].set_title("Training Loss")
+    axs[1, 0].plot(train_acc)
+    axs[1, 0].set_title("Training Accuracy")
+    axs[0, 1].plot(test_losses)
+    axs[0, 1].set_title("Test Loss")
+    axs[1, 1].plot(test_acc)
+    axs[1, 1].set_title("Test Accuracy")
